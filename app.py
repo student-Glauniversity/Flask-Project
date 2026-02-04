5824f5738708a7fef89c1c6fe51dff8a99de2cf2
parth <parth8619agrawal@gmail.com>
2026-02-04 15:12:10 +0530

    initial commit

---
+++ app.py
@@ -0,0 +1,67 @@
+from flask import Flask, render_template, request ,redirect
+from flask_sqlalchemy import SQLAlchemy
+
+app = Flask(__name__)
+
+app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///employee.db"
+app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+
+db = SQLAlchemy(app)
+
+class Employee(db.Model):
+    sno = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(200), nullable=False)
+    email = db.Column(db.String(500), nullable=False)
+
+    def __repr__(self):
+        return f"{self.sno} - {self.name}"
+
+
+@app.route("/", methods=['GET', 'POST'])
+def home():
+    if request.method == 'POST':
+        name = request.form['name']
+        email = request.form['email']
+
+        employee = Employee(name=name, email=email)
+        db.session.add(employee)
+        db.session.commit()
+
+    allemployees = Employee.query.all()
+    return render_template("index.html", allemployees=allemployees)
+
+@app.route("/delete/<int:sno>")
+def delete(sno):
+    employee = Employee.query.filter_by(sno=sno).first()
+    db.session.delete(employee)
+    db.session.commit()
+    return redirect("/")
+@app.route("/update/<int:sno>",methods=['GET', 'POST'])
+def update(sno):
+    if request.method == 'POST':
+        name = request.form['name']
+        email = request.form['email']
+
+        employee = Employee.query.filter_by(sno=sno).first()
+        employee.name = name
+        employee.email = email
+        db.session.add(employee)
+        db.session.commit()
+        return redirect("/")
+
+    employee = Employee.query.filter_by(sno=sno).first()
+    return render_template("update.html", employee=employee)
+    
+
+
+
+
+@app.route("/about")
+def about():
+    return render_template("about.html")
+
+
+if __name__ == "__main__":
+    with app.app_context():
+        db.create_all()
+    app.run(debug=True)
